c6c0ec5612426b05e4f279ef8e8dccf1ed8da2eb
Saksham-Batra <140792075+Saksham-Batra@users.noreply.github.com>
2024-04-09 19:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	if(num==1){
+		return 0;
+	}
+	if(num==2){
+return 1;
+	}
+	let prev = 0;
+	let curr = 1;
+	let next;
+	for(let i = 3; i<=num; i++){
+next = prev+curr;
+		prev = curr;
+		curr = next;
+	}
+	return next;
 }
 
 module.exports = fibonacci;
